d923e69657f709be735ed4ffcb895bf8e664fa83
SiddhantSingh1234 <siddhantsingh.15032005@gmail.com>
2026-01-28 15:36:58 +0530

    Last commit

--- python_connectivity.py
+++ python_connectivity.py
@@ -3,7 +3,7 @@ import psycopg2
 # Name: Siddhant Singh
 # Roll Number: 23CS10085
 # Department: Computer Science and Engineering
-# DBMS Lab
+# Database Management Lab
 # Assignment Number: 3
 
 conn = psycopg2.connect(
